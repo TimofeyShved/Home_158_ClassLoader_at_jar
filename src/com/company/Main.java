d2e84d8b1f6bf4ee6dbbfa3f6058be1128cc6696
TimofeyShved <timofeya123@inbox.ru>
2023-12-29 11:35:38 +0600

    Class loader

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,38 @@
 package com.company;
 
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLClassLoader;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 public class Main {
 
-    public static void main(String[] args) {
-	// write your code here
+    public static void main(String[] args) throws MalformedURLException, ClassNotFoundException {
+        // Подключение библиотек и различных классов (JAR)
+        // Указываем путь и загружаем его в загрузчик классов
+        URL myUrl = new URL("C://Users/Admin/IdeaProjects/Home_158_ClassLoader_at_jar/MyJar.jar");
+        URLClassLoader pluginClassLoader = new URLClassLoader(new URL[]{ myUrl });
+
+        // Вытаскиваем класс из загрузчика
+        Class myClass = pluginClassLoader.loadClass("MyClassName");
+    }
+}
+
+// реализуем свою логику в загрузчике (не обязательно)
+class MyLoader extends ClassLoader{
+    @Override
+    protected Class<?> findClass(String name) throws ClassNotFoundException {
+        // считыает массив байтов (при желании можно их сдвигать или шифровать, например что бы защитить от декадирования)
+        byte[] bytes = new byte[0];
+        try {
+            bytes= Files.readAllBytes(Paths.get("filename"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        Class<?> cl = defineClass(name, bytes, 0, bytes.length);
+        return cl;
     }
 }
